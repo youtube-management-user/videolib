a343bef5f21c3cd9aa0602bf5291e4d4ec8ce09d
Pavel Filippov <zanziver@gmail.com>
2021-01-27 20:28:26 +0100

    upd

--- libs/utils.js
+++ libs/utils.js
@@ -3,11 +3,6 @@ const fs = require('fs');
 //const { parse } = require('fecha');
 const fetch = require('node-fetch');
 const _ = require('lodash');
-const glob = require('glob');
-
-const lecturesMapping = [ 'ФИ', 'ЯК', 'АЭ', 'ИО', 'HM' ];
-const lecturesHeadersMapping = [ 'Философия искусства', 'Языки культуры', 'Анаморфическая энциклопедия', 'Идеи и образы', 'Homo mutabilis' ];
-const coursesLatinMapping = [ 'FI', 'YK', 'AE', 'IO', 'HM' ];
 
 function csv(file) {
   if (!file) return null;
@@ -29,6 +24,8 @@ function csv(file) {
 
 function buildPlaylist() {
   let playlist = {};
+  const lecturesMapping = [ 'ФИ', 'ЯК', 'АЭ', 'ИО', 'HM' ];
+  const coursesLatinMapping = { 'ФИ': 'FI', 'ЯК' : 'YK', 'АЭ': 'AE', 'ИО': 'IO', 'HM': 'HM' };
 
   let youtubeMapping = {};
   for (var i=1; i<=5; i++) {
@@ -166,57 +163,4 @@ async function syncPaidFileStatuses() {
   }
 }
 
-function convertTextFiles() {
-
-  const result = [];
-  for (var i=1; i<=5; i++) {
-    glob(`./txt/${i}/*.txt`, {}, function(err, files) {
-      if (files.length > 0) {
-        files.forEach(path => {
-          const pathArr = path.split('/');
-          const course = pathArr[pathArr.length-2];
-          filename = pathArr[pathArr.length-1];
-          const number = filename.replace('.txt', '');
-
-          let lectureData = {};
-
-          var data = fs.readFileSync(path, 'UTF-8').split(/\r\n\r\n|\n\n/).filter(l => l!='');
-
-          data.forEach(part => {
-            let [key, ...value] = part.split(/[\r]*\n/);
-            key = key.replace(/[^a-zA-Z+]/, '');
-            value = value.filter(v => v!='');
-            if (value!='') {
-              if (value.length == 1) {
-                value = value[0];
-                if (value.replace(/^\s+\|\s+$/g, '')=='')
-                  value = '';
-              }
-              lectureData[key] = value;
-            }
-          });
-
-          lectureData.courseHeader = lecturesHeadersMapping[course-1];
-          lectureData.course = parseInt(course);
-          lectureData.courseLetters = lecturesMapping[course-1];
-          lectureData.link = `${course}/${number}`;
-          lectureData.video = `${coursesLatinMapping[course-1]}-${number}`;
-          lectureData.courseGlobalNum = parseInt(course) + 13;
-          lectureData.number = parseInt(number);
-          if (lectureData.literature) {
-            lectureData.literature = lectureData.literature.map(l => l.split('|'));
-          }
-          if (lectureData.movies) {
-            lectureData.movies = lectureData.movies.map(l => l.split('|'));
-          }
-
-          result.push(lectureData);
-
-        })
-      }
-      fs.writeFileSync('./txt/lectures.json', JSON.stringify(result, null, 2));
-    });
-  }
-}
-
-module.exports = { csv, buildPlaylist, getOpenOrders, reloadPaidFile, syncPaidFileStatuses, convertTextFiles }
+module.exports = { csv, buildPlaylist, getOpenOrders, reloadPaidFile, syncPaidFileStatuses }
