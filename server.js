0b3b4e0296eea603d1e4bf7ca9dff3299995468f
Pavel Filippov <zanziver@gmail.com>
2021-01-31 01:00:19 +0100

    upd

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const initAuth          = require('./routes/auth.js');
 
 let playlist = buildPlaylist();
 
-reloadPaidFile()
+setInterval(reloadPaidFile, 1000* 60 * 5);
 
 setInterval(syncPaidFileStatuses, 1000* 60 * 5);
 
