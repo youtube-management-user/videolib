d6a47e10f07b9796dad0e1214a0151809c3829b5
Pavel Filippov <zanziver@gmail.com>
2021-09-09 09:09:26 +0200

    fix youtube parse in local_id

--- libs/utils.js
+++ libs/utils.js
@@ -1,25 +1,27 @@
-
-const fs = require('fs');
+const fs = require("fs");
 //const { parse } = require('fecha');
-const fetch = require('node-fetch');
-const _ = require('lodash');
+const fetch = require("node-fetch");
+const _ = require("lodash");
 
 function csv(file, fields) {
-  if (!file) { console.error('empty file'); return null; }
+  if (!file) {
+    console.error("empty file");
+    return null;
+  }
   let records;
   if (!fields) {
-    [fields, ...records] = file.split(/\n/).filter(l => l!='');
-    fields = fields.replace(/[\n\r\s]+/g, '').split(/[|,]/);
+    [fields, ...records] = file.split(/\n/).filter(l => l != "");
+    fields = fields.replace(/[\n\r\s]+/g, "").split(/[|,]/);
   } else {
-    records = file.split(/\n/).filter(l => l!='');
+    records = file.split(/\n/).filter(l => l != "");
   }
 
   if (!records || records.length < 1) return null;
 
   records = records.map(rec => {
     let res = {};
-    let values = rec.replace(/[\n\r]+/g, '').split('|');
-    values.forEach((val, ind) => res[fields[ind]] = values[ind]);
+    let values = rec.replace(/[\n\r]+/g, "").split("|");
+    values.forEach((val, ind) => (res[fields[ind]] = values[ind]));
     return res;
   });
 
@@ -28,21 +30,43 @@ function csv(file, fields) {
 
 function buildPlaylist() {
   let playlist = {};
-  const lecturesMapping = [ 'ФИ', 'ЯК', 'АЭ', 'ИО', 'HM' ];
-  const coursesLatinMapping = { 'ФИ': 'FI', 'ЯК' : 'YK', 'АЭ': 'AE', 'ИО': 'IO', 'HM': 'HM' };
+  const lecturesMapping = ["ФИ", "ЯК", "АЭ", "ИО", "HM"];
+  const coursesLatinMapping = {
+    ФИ: "FI",
+    ЯК: "YK",
+    АЭ: "AE",
+    ИО: "IO",
+    HM: "HM"
+  };
 
   let youtubeMapping = {};
-  for (var i=1; i<=5; i++) {
-    const path = `./local_id/local_${i}_id.txt`
+  for (var i = 1; i <= 5; i++) {
+    const path = `./local_id/local_${i}_id.txt`;
     if (fs.existsSync(path)) {
-      let youtubeIds = fs.readFileSync(path, 'UTF-8').split(/[\n\r]+/).filter(l => l!='');
-      youtubeMapping[lecturesMapping[i-1]] = youtubeIds;
-      youtubeIds.forEach((id,ind) => {
-        const [youtube, local, medium, low] = id.split('|');
-        const type = (youtube=='' && (local!='' || medium!='' || low!=''))? { type: 'local', path: local? './videos/' + local: null, medium: medium? './videos/' + medium: null, low: low? './videos/' + low: null }: { type: 'youtube', url: youtube }
-        const paddedNum = ind+1 < 10? `0${ind+1}`: ind+1
-        playlist[`${coursesLatinMapping[lecturesMapping[i-1]]}-${paddedNum}`] = type;
-      })
+      let youtubeIds = fs
+        .readFileSync(path, "UTF-8")
+        .split(/[\n\r]+/)
+        .filter(l => l != "");
+      youtubeMapping[lecturesMapping[i - 1]] = youtubeIds;
+      youtubeIds.forEach((id, ind) => {
+        let [youtube, local, medium, low] = id.split("|");
+        if (youtube) {
+          youtube = youtube.replace(/\s/, "");
+        }
+        const type =
+          youtube == "" && (local != "" || medium != "" || low != "")
+            ? {
+                type: "local",
+                path: local ? "./videos/" + local : null,
+                medium: medium ? "./videos/" + medium : null,
+                low: low ? "./videos/" + low : null
+              }
+            : { type: "youtube", url: youtube };
+        const paddedNum = ind + 1 < 10 ? `0${ind + 1}` : ind + 1;
+        playlist[
+          `${coursesLatinMapping[lecturesMapping[i - 1]]}-${paddedNum}`
+        ] = type;
+      });
     }
   }
   return playlist;
@@ -50,77 +74,107 @@ function buildPlaylist() {
 
 function parse(date, daysToAdd) {
   var parts = date.split(".");
-  var dt = new Date(parseInt(parts[2], 10),
-                    parseInt(parts[1], 10) - 1,
-                    parseInt(parts[0], 10) + (daysToAdd || 0),
-                    // parseInt(parts[3], 10),
-                    // parseInt(parts[4], 10),
-                  );
+  var dt = new Date(
+    parseInt(parts[2], 10),
+    parseInt(parts[1], 10) - 1,
+    parseInt(parts[0], 10) + (daysToAdd || 0)
+    // parseInt(parts[3], 10),
+    // parseInt(parts[4], 10),
+  );
   return dt;
 }
 
 async function getOpenOrders(email) {
-
   function emailInGroup(groups, email, statuses) {
-    statuses = statuses.split('');
-    if (!groups || !groups[0]) { console.error('emailInGroup group cache is empty'); return false; }
-    if (!email) { console.error('emailInGroup email is empty'); return false; }
-    return groups[0].members.filter(m => m.email.toLowerCase() == email.toLowerCase() && parseInt(statuses[parseInt(m.num)-1]) == 1).length > 0;
+    statuses = statuses.split("");
+    if (!groups || !groups[0]) {
+      console.error("emailInGroup group cache is empty");
+      return false;
+    }
+    if (!email) {
+      console.error("emailInGroup email is empty");
+      return false;
+    }
+    return (
+      groups[0].members.filter(
+        m =>
+          m.email.toLowerCase() == email.toLowerCase() &&
+          parseInt(statuses[parseInt(m.num) - 1]) == 1
+      ).length > 0
+    );
   }
 
-  if (!fs.existsSync('./txt/orders.json') || !fs.existsSync('./txt/groups.json')) {
+  if (
+    !fs.existsSync("./txt/orders.json") ||
+    !fs.existsSync("./txt/groups.json")
+  ) {
     try {
       await reloadPaidFile();
       await convertGroupFiles();
-    } catch(ex) {
+    } catch (ex) {
       return null;
     }
   } else {
-
     let currentOrder = null;
     try {
-      const orders = JSON.parse(fs.readFileSync('./txt/orders.json', 'UTF-8'));
-      const groups = JSON.parse(fs.readFileSync('./txt/groups.json', 'UTF-8'));
+      const orders = JSON.parse(fs.readFileSync("./txt/orders.json", "UTF-8"));
+      const groups = JSON.parse(fs.readFileSync("./txt/groups.json", "UTF-8"));
 
-//      console.log(orders.filter(o => o.name.indexOf('group') >=0))
+      //      console.log(orders.filter(o => o.name.indexOf('group') >=0))
 
       let openOrders = [];
       openOrders = orders.filter(order => {
-        const isTime = new Date(order.begin) <= new Date() && new Date() <= new Date(order.end);
-        const isPersonalOrder = order.gmail && email && order.gmail.toLowerCase().trim() == email.toLowerCase().trim() && (parseInt(order.okl) === 1 || parseInt(order.okl) === 2 || parseInt(order.okl) === 6);
+        const isTime =
+          new Date(order.begin) <= new Date() &&
+          new Date() <= new Date(order.end);
+        const isPersonalOrder =
+          order.gmail &&
+          email &&
+          order.gmail.toLowerCase().trim() == email.toLowerCase().trim() &&
+          (parseInt(order.okl) === 1 ||
+            parseInt(order.okl) === 2 ||
+            parseInt(order.okl) === 6);
         const isOpenForEveryone = parseInt(order.okl) === 4;
-        const isOpenForGroup = order.name.indexOf('group') == 0 && (parseInt(order.okl) === 1 || parseInt(order.okl) === 2) && emailInGroup(groups.filter(group => group.title == order.name), email, order.nameo);
-//        console.log(isTime, order.gmail, order.gmail.toLowerCase(), email.toLowerCase())
-//        if (isTime) console.log(order.nameo.indexOf('group') == 0)
-        return (isPersonalOrder || isOpenForEveryone || isOpenForGroup) && isTime;
+        const isOpenForGroup =
+          order.name.indexOf("group") == 0 &&
+          (parseInt(order.okl) === 1 || parseInt(order.okl) === 2) &&
+          emailInGroup(
+            groups.filter(group => group.title == order.name),
+            email,
+            order.nameo
+          );
+        //        console.log(isTime, order.gmail, order.gmail.toLowerCase(), email.toLowerCase())
+        //        if (isTime) console.log(order.nameo.indexOf('group') == 0)
+        return (
+          (isPersonalOrder || isOpenForEveryone || isOpenForGroup) && isTime
+        );
       });
 
       return openOrders;
-    } catch(ex) {
-      console.log(ex)
+    } catch (ex) {
+      console.log(ex);
       return null;
-    };
+    }
   }
 }
 
 async function fetchPaidFile() {
   let orders = [];
   try {
-    const response = await fetch('http://velikanov.ru/txt/paid_h.txt');
+    const response = await fetch("http://velikanov.ru/txt/paid_h.txt");
     let body = await response.buffer();
-    body = body.toString('utf16le');
-//    body = fs.readFileSync('./txt/paid_h.txt', 'UTF-8')
+    body = body.toString("utf16le");
+    //    body = fs.readFileSync('./txt/paid_h.txt', 'UTF-8')
     let orders = csv(body);
-    console.log(orders[orders.length-1])
-    orders = orders
-    .map(rec => {
-      rec.begin = parse(rec.begin.split(' ')[0]);
-      rec.end = parse(rec.end, 1); return rec;
+    console.log(orders[orders.length - 1]);
+    orders = orders.map(rec => {
+      rec.begin = parse(rec.begin.split(" ")[0]);
+      rec.end = parse(rec.end, 1);
+      return rec;
     });
     return orders;
-  }
-  catch(ex) {
-    console.log('Error while fetching paid_h', ex);
+  } catch (ex) {
+    console.log("Error while fetching paid_h", ex);
     return;
   }
 }
@@ -128,59 +182,70 @@ async function fetchPaidFile() {
 async function reloadPaidFile() {
   let orders = await fetchPaidFile();
   if (orders && orders.length > 0) {
-    console.log('Writing new orders cache')
-    fs.writeFileSync('./txt/orders.json', JSON.stringify(orders));
+    console.log("Writing new orders cache");
+    fs.writeFileSync("./txt/orders.json", JSON.stringify(orders));
     return orders;
-  }
-  else return null;
+  } else return null;
 }
 
 let syncPaidFileStatusesRunning = false;
 
 async function syncPaidFileStatuses() {
-
   // const domain = 'http://localhost:9300/test';
-  const domain = 'http://velikanov.ru';
+  const domain = "http://velikanov.ru";
 
   try {
     const response = await fetch(domain);
-  } catch(ex) {
-    console.error(`Domain ${domain} is unreachable, cannot sync orders status ${ex}`);
+  } catch (ex) {
+    console.error(
+      `Domain ${domain} is unreachable, cannot sync orders status ${ex}`
+    );
     return;
   }
 
   if (syncPaidFileStatusesRunning === false) {
     syncPaidFileStatusesRunning = true;
     await reloadPaidFile();
-//    let orders = await fetchPaidFile();
-    const orders = JSON.parse(fs.readFileSync('./txt/orders.json', 'UTF-8'));
-    let openOrders = orders.filter(order => (parseInt(order.okl) === 1) && (new Date(order.begin) <= new Date() && new Date() <= new Date(order.end)));
-    let closedOrders = orders.filter(order => (parseInt(order.okl) === 1 || parseInt(order.okl) === 2 || parseInt(order.okl) === 4) && (new Date() > new Date(order.end)));
+    //    let orders = await fetchPaidFile();
+    const orders = JSON.parse(fs.readFileSync("./txt/orders.json", "UTF-8"));
+    let openOrders = orders.filter(
+      order =>
+        parseInt(order.okl) === 1 &&
+        new Date(order.begin) <= new Date() &&
+        new Date() <= new Date(order.end)
+    );
+    let closedOrders = orders.filter(
+      order =>
+        (parseInt(order.okl) === 1 ||
+          parseInt(order.okl) === 2 ||
+          parseInt(order.okl) === 4) &&
+        new Date() > new Date(order.end)
+    );
 
     let openOrderLinks = openOrders.map(order => {
-  //    const id = orders.findIndex(o => o.id == order.id) + 2;
-      return  `${domain}/read_orders_hand6.asp?a=6&b=2&move=0&tid=${order.id}&pass=Bgt57140135`
+      //    const id = orders.findIndex(o => o.id == order.id) + 2;
+      return `${domain}/read_orders_hand6.asp?a=6&b=2&move=0&tid=${order.id}&pass=Bgt57140135`;
     });
 
-    console.log('openOrderLinks', openOrderLinks)
+    console.log("openOrderLinks", openOrderLinks);
 
     let closedOrderLinks = closedOrders.map(order => {
-//      const id = orders.findIndex(o => o.id == order.id) + 2;
-      return  `${domain}/read_orders_hand6.asp?a=6&b=3&move=0&tid=${order.id}&pass=Bgt57140135`
+      //      const id = orders.findIndex(o => o.id == order.id) + 2;
+      return `${domain}/read_orders_hand6.asp?a=6&b=3&move=0&tid=${order.id}&pass=Bgt57140135`;
     });
 
-    console.log('closedOrderLinks', closedOrderLinks)
+    console.log("closedOrderLinks", closedOrderLinks);
 
     let links = openOrderLinks.concat(closedOrderLinks);
 
     for (const link of links) {
-      if (link!='' && link.indexOf('http')>=0) {
-        let linkText = link.replace(/(pass\=[^&]+)/g, '');
-        console.log(`Calling ${linkText}`)
+      if (link != "" && link.indexOf("http") >= 0) {
+        let linkText = link.replace(/(pass\=[^&]+)/g, "");
+        console.log(`Calling ${linkText}`);
         try {
           const response = await fetch(link);
           console.log(`Response is ${response.status}`);
-        } catch(ex) {
+        } catch (ex) {
           console.log(`Error ${ex}`);
         }
       }
@@ -191,89 +256,120 @@ async function syncPaidFileStatuses() {
 }
 
 function convertTextFiles() {
-  const fs = require('fs');
-  const glob = require('glob');
+  const fs = require("fs");
+  const glob = require("glob");
 
-  const lecturesMapping = [ 'ФИ', 'ЯК', 'АЭ', 'ИО', 'HM' ];
-  const lecturesHeadersMapping = [ 'Философия искусства', 'Языки культуры', 'Анаморфическая энциклопедия', 'Идеи и образы', 'Homo mutabilis' ];
-  const coursesLatinMapping = [ 'FI', 'YK', 'AE', 'IO', 'HM' ];
+  const lecturesMapping = ["ФИ", "ЯК", "АЭ", "ИО", "HM"];
+  const lecturesHeadersMapping = [
+    "Философия искусства",
+    "Языки культуры",
+    "Анаморфическая энциклопедия",
+    "Идеи и образы",
+    "Homo mutabilis"
+  ];
+  const coursesLatinMapping = ["FI", "YK", "AE", "IO", "HM"];
 
   const result = [];
   var files = glob.sync(`./data/[1-5]*/*.txt`);
   files.forEach(path => {
-    const pathArr = path.split('/');
-    const course = pathArr[pathArr.length-2];
-    filename = pathArr[pathArr.length-1];
-    const number = filename.replace('.txt', '');
+    const pathArr = path.split("/");
+    const course = pathArr[pathArr.length - 2];
+    filename = pathArr[pathArr.length - 1];
+    const number = filename.replace(".txt", "");
 
     let lectureData = {};
 
-    var data = fs.readFileSync(path, 'UTF-8').split(/\r\n\r\n|\n\n/).filter(l => l!='');
+    var data = fs
+      .readFileSync(path, "UTF-8")
+      .split(/\r\n\r\n|\n\n/)
+      .filter(l => l != "");
 
     data.forEach(part => {
-      part = part.replace(/^[^a-zA-Z]+/, '');
+      part = part.replace(/^[^a-zA-Z]+/, "");
       let [key, ...value] = part.split(/[\r]*\n/);
-      key = key.replace(/[^a-zA-Z+]/, '');
-      value = value.filter(v => v!='');
-//          console.log(222, key, value)
-      if (value!='') {
+      key = key.replace(/[^a-zA-Z+]/, "");
+      value = value.filter(v => v != "");
+      //          console.log(222, key, value)
+      if (value != "") {
         if (value.length == 1) {
           value = value[0];
-          if (value.replace(/^\s+\|\s+$/g, '')=='')
-            value = '';
+          if (value.replace(/^\s+\|\s+$/g, "") == "") value = "";
         }
         lectureData[key] = value;
       }
     });
 
-    lectureData.courseHeader = lecturesHeadersMapping[course-1];
+    lectureData.courseHeader = lecturesHeadersMapping[course - 1];
     lectureData.course = parseInt(course);
-    lectureData.courseLetters = lecturesMapping[course-1];
+    lectureData.courseLetters = lecturesMapping[course - 1];
     lectureData.link = `${course}/${number}`;
-    const paddedNum = parseInt(number) > 9? number: 0+''+parseInt(number);
-    lectureData.video = `${coursesLatinMapping[course-1]}-${paddedNum}`;
+    const paddedNum = parseInt(number) > 9 ? number : 0 + "" + parseInt(number);
+    lectureData.video = `${coursesLatinMapping[course - 1]}-${paddedNum}`;
     lectureData.courseGlobalNum = parseInt(course) + 13;
     lectureData.number = parseInt(number);
     if (lectureData.literature) {
-      lectureData.literature = lectureData.literature.map(l => l.split('|'));
+      lectureData.literature = lectureData.literature.map(l => l.split("|"));
     }
     if (lectureData.movies) {
-      if (typeof lectureData.movies == 'string') {
-        lectureData.movies = [ lectureData.movies ]
+      if (typeof lectureData.movies == "string") {
+        lectureData.movies = [lectureData.movies];
       }
-      lectureData.movies = lectureData.movies.map(l => l.split('|'));
+      lectureData.movies = lectureData.movies.map(l => l.split("|"));
     }
 
     result.push(lectureData);
-
-  })
-  fs.writeFileSync('./txt/lectures.json', JSON.stringify(result, null, 2));
+  });
+  fs.writeFileSync("./txt/lectures.json", JSON.stringify(result, null, 2));
 }
 
 function convertGroupFiles() {
-  const fs = require('fs');
-  const glob = require('glob');
+  const fs = require("fs");
+  const glob = require("glob");
   var files = glob.sync(`./groups/group*.txt`);
   var groups = [];
   files.forEach(path => {
-    var data = fs.readFileSync(path, 'UTF-8');//.split().split(/\r\n\r\n|\n\n/).filter(l => l!='');
-    var title = path.split('/'); title = title[title.length-1].split('.')[0];
-    groups.push({ title: title, members: csv(data, ['num','lastname','firstname','email'])});
-//    data.map(l => l.split('|'))
-//    groups.push();
-  })
-  fs.writeFileSync('./txt/groups.json', JSON.stringify(groups, null, 2));
+    var data = fs.readFileSync(path, "UTF-8"); //.split().split(/\r\n\r\n|\n\n/).filter(l => l!='');
+    var title = path.split("/");
+    title = title[title.length - 1].split(".")[0];
+    groups.push({
+      title: title,
+      members: csv(data, ["num", "lastname", "firstname", "email"])
+    });
+    //    data.map(l => l.split('|'))
+    //    groups.push();
+  });
+  fs.writeFileSync("./txt/groups.json", JSON.stringify(groups, null, 2));
 }
 
 function getOrderByURL(orders, url) {
-  const coursesLatinMapping = { 'FI': 'ФИ', 'YK': 'ЯК' ,'AE': 'АЭ', 'IO': 'ИО', 'HM': 'HM' };
-  if (orders && url && url!='' && url.indexOf('/') >=0) {
-    const [course, number] = url.split('/')[2].split('-');
-//    console.log(333, course, number, orders)
-    return orders.find(order => { return order.course == coursesLatinMapping[course] && parseInt(order.number) == parseInt(number) });
+  const coursesLatinMapping = {
+    FI: "ФИ",
+    YK: "ЯК",
+    AE: "АЭ",
+    IO: "ИО",
+    HM: "HM"
+  };
+  if (orders && url && url != "" && url.indexOf("/") >= 0) {
+    const [course, number] = url.split("/")[2].split("-");
+    //    console.log(333, course, number, orders)
+    return orders.find(order => {
+      return (
+        order.course == coursesLatinMapping[course] &&
+        parseInt(order.number) == parseInt(number)
+      );
+    });
   } else {
     return null;
   }
 }
 
-module.exports = { csv, buildPlaylist, getOpenOrders, reloadPaidFile, syncPaidFileStatuses, convertTextFiles, convertGroupFiles, getOrderByURL }
+module.exports = {
+  csv,
+  buildPlaylist,
+  getOpenOrders,
+  reloadPaidFile,
+  syncPaidFileStatuses,
+  convertTextFiles,
+  convertGroupFiles,
+  getOrderByURL
+};
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,43 +1,49 @@
-
-const cookie = require('cookie');
+const cookie = require("cookie");
 const ejs = require("ejs");
-const fs = require('fs');
-const _ = require('lodash');
+const fs = require("fs");
+const _ = require("lodash");
 
-const { getOpenOrders, convertTextFiles } = require('../libs/utils.js');
+const { getOpenOrders, convertTextFiles } = require("../libs/utils.js");
 
-const { urlGoogle } = require('../libs/google-utils.js');
+const { urlGoogle } = require("../libs/google-utils.js");
 
 async function playlistRoute(req, res, course, number, playlist) {
-
-  const lecturesPath = './txt/lectures.json';
+  const lecturesPath = "./txt/lectures.json";
   if (!fs.existsSync(lecturesPath)) {
     convertTextFiles();
   }
 
-  const lectures = JSON.parse(fs.readFileSync(lecturesPath, 'UTF-8'));
+  const lectures = JSON.parse(fs.readFileSync(lecturesPath, "UTF-8"));
 
   let openOrders = [];
 
-  const userEmail = (req.user && req.user.email)? req.user.email: null;
+  const userEmail = req.user && req.user.email ? req.user.email : null;
 
   openOrders = await getOpenOrders(userEmail);
 
-  let openLectures = _.uniq(openOrders.map(order => {
-    let lecture = lectures.find(l => l.courseLetters == order.course && l.number == order.number )
-    const res = lecture || order
-    res.speedChangeAllowed = parseInt(order.v) === 1;
-    return res;
-  }));
+  let openLectures = _.uniq(
+    openOrders.map(order => {
+      let lecture = lectures.find(
+        l => l.courseLetters == order.course && l.number == order.number
+      );
+      const res = lecture || order;
+      res.speedChangeAllowed = parseInt(order.v) === 1;
+      return res;
+    })
+  );
 
   let lectureData = {};
 
   if (req.user && req.user.email) {
-    lectureData = openLectures.find(l => l.course == course && l.number == number);
+    lectureData = openLectures.find(
+      l => l.course == course && l.number == number
+    );
   }
 
   let pageTitle = null;
-  const currentLectureDataForTitle = openLectures.find(l => l.course == course && l.number == number);
+  const currentLectureDataForTitle = openLectures.find(
+    l => l.course == course && l.number == number
+  );
   if (currentLectureDataForTitle) {
     pageTitle = `${currentLectureDataForTitle.courseHeader}.${currentLectureDataForTitle.number}. ${currentLectureDataForTitle.title}`;
   }
@@ -45,28 +51,38 @@ async function playlistRoute(req, res, course, number, playlist) {
   let playlistItem;
 
   if (lectureData && lectureData.video) {
-    playlistItem = playlist[lectureData.video]
+    playlistItem = playlist[lectureData.video];
     if (fs.existsSync(`./templates/img/${lectureData.video}.png`)) {
       lectureData.poster = `/img/${lectureData.video}.png`;
-      console.log(`Found poster ${lectureData.video}.png`)
+      console.log(`Found poster ${lectureData.video}.png`);
     } else {
-      console.log(`Not found poster ${lectureData.video}.png`)
+      console.log(`Not found poster ${lectureData.video}.png`);
     }
 
     playlistItem.amount = 0;
     if (playlistItem.low) playlistItem.amount++;
     if (playlistItem.medium) playlistItem.amount++;
     if (playlistItem.path) playlistItem.amount++;
-
   }
 
   let showAuthorisationLink = true;
 
   const googleLink = urlGoogle({ redirect: req.url });
 
-  console.log(111, lectureData, playlistItem)
+  //  console.log(111, lectureData, playlistItem)
 
-  var contents = ejs.render(fs.readFileSync("./templates/playlist.ejs", 'UTF-8'), { user: req.user, openLectures, playlistItem, googleLink, lectureData, pageTitle, showAuthorisationLink });
+  var contents = ejs.render(
+    fs.readFileSync("./templates/playlist.ejs", "UTF-8"),
+    {
+      user: req.user,
+      openLectures,
+      playlistItem,
+      googleLink,
+      lectureData,
+      pageTitle,
+      showAuthorisationLink
+    }
+  );
 
   res.setHeader("Content-Type", "text/html; charset=utf-8");
   res.writeHead(200);
